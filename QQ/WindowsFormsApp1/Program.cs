e345149b9e3e7817b1c91c2dcb13e59a25225fee
kugeorge <jpcgeorge@gmail.com>
2020-07-10 20:08:50 -0400

    abc

--- QQ/WindowsFormsApp1/Program.cs
+++ QQ/WindowsFormsApp1/Program.cs
@@ -14,7 +14,7 @@ namespace WindowsFormsApp1
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+           // Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
